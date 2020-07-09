069412c3a23c83f99e0283d41e28738f01485096
ansheng1021 <ansheng1021@163.com>
2020-07-09 11:31:03 +0800

    添加header

---
+++ src/components/layouts/TheHeader.vue
@@ -0,0 +1,33 @@
+<template>
+    <div class="navbar navbar-default topnav">
+        <div class="container">
+            <div class="navbar-header">
+                <a href="/" class="navbar-brand">
+                    <span class="title">{{ logo.title }}</span>
+                    <img :src="logo.src" :alt="logo.title">
+                </a>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: 'TheHeader',
+        data() {
+            return {
+                logo: {
+                    src: `${this.uploadsUrl}/src/assets/components/header/images/logo_02-8ef07cc6ad.png`,
+                    title: '30秒懂车 Vue.js'
+                }
+            }
+        },
+        beforeCreate() {
+            this.uploadsUrl = 'https://www.30sche.com'
+        }
+    }
+</script>
+
+<style scoped>
+    .title { display: none;}
+</style>
