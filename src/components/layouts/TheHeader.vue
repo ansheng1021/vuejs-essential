a38ff0dd9dfa0f1c7019521ce6b5ff1a1652148b
ansheng1021 <ansheng1021@163.com>
2020-07-09 11:43:14 +0800

    兼容移动端

--- src/components/layouts/TheHeader.vue
+++ src/components/layouts/TheHeader.vue
@@ -2,14 +2,30 @@
     <div class="navbar navbar-default topnav">
         <div class="container">
             <div class="navbar-header">
+                <button type="button" class="navbar-toggle" @click="toggleNav">
+                    <span class="sr-only">Toggle navigation</span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                </button>
+
                 <a href="/" class="navbar-brand">
                     <span class="title">{{ logo.title }}</span>
                     <img :src="logo.src" :alt="logo.title">
                 </a>
             </div>
+
+            <div id="top-navbar-collapse" :class="['collapse', 'navbar-collapse', { in: showCollapsedNav }]">
+                <ul class="nav navbar-nav">
+                    <li v-for="(item, index) in navList" :class="{ active: index === activeNavIndex }">
+                        <a href="#" @click="changeNavIndex(index)">{{ item }}</a>
+                    </li>
+                </ul>
+            </div>
         </div>
     </div>
 </template>
+
 
 <script>
     export default {
@@ -19,15 +35,29 @@
                 logo: {
                     src: `${this.uploadsUrl}/src/assets/components/header/images/logo_02-8ef07cc6ad.png`,
                     title: '30秒懂车 Vue.js'
-                }
+                },
+                navList: ['社区', '头条', '问答', '教程'],
+                activeNavIndex: 0,
+                showCollapsedNav: false
             }
         },
         beforeCreate() {
             this.uploadsUrl = 'https://www.30sche.com'
+        },
+        methods: {
+            changeNavIndex(index) {
+                this.activeNavIndex = index
+            },
+            toggleNav() {
+                this.showCollapsedNav = !this.showCollapsedNav
+            }
         }
     }
 </script>
 
 <style scoped>
-    .title { display: none;}
+    .title {
+        display: none;
+    }
+    .navbar-default .navbar-nav > .active > a { background: rgba(0,0,0,.03);}
 </style>
