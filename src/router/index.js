4b1940cbece2b8fdc860ad0894c27ef3ec6ebc6b
ansheng1021 <ansheng1021@163.com>
2020-07-09 14:43:12 +0800

    添加弹窗组件和退出登录

--- src/router/index.js
+++ src/router/index.js
@@ -8,6 +8,18 @@ const routes = [
         path: '/auth/register',
         name: 'Register',
         component: () => import('@/views/auth/Register')   //懒加载
+    },
+    // 首页路由配置
+    {
+        path: '/',
+        name: 'Home',
+        component: () => import('@/views/Home')
+    },
+    // 其他未配置的路由都跳转到首页
+    {
+        path: '*',
+        // 重定向
+        redirect: '/'
     }
 ]
 
@@ -15,5 +27,14 @@ const router = new Router({
     mode: 'history',
     routes
 })
+// 全局前置守卫
+router.beforeEach((to, from, next) => {
+    const auth = router.app.$options.store.state.auth
 
+    if (auth && to.path.indexOf('/auth/') !== -1) {
+        next('/')
+    } else {
+        next()
+    }
+})
 export default router
